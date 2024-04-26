42165bc9b6298ad0fc851ee0fea1e734c9442a49
lythanh00 <123355879+lythanh00@users.noreply.github.com>
2024-04-26 10:42:54 +0700

    delete backend
    delete backend

--- backendFoodGate/src/main/java/com/example/footgate/config/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-//package com.example.footgate.config;
-//
-//public class JwtConstant {
-//    public static final String SECRET_KEY="randomstring;asdasafasasdsds";
-//    public static final String JWT_HEADER = "Authorization";
-//}
--- backendFoodGate/src/main/java/com/example/footgate/request/UpdateCartItemRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.footgate.request;
-
-import lombok.Data;
-
-@Data
-public class UpdateCartItemRequest {
-    private Long cartItemId;
-    private int quantity;
-}
