ae2299f96183ef458b7f8686af1d9b306513cb55
01KingArthur <minhcuongdo01@gmail.com>
2024-04-27 17:27:00 +0700

    complete AdminUI

--- frontend-food-ordering/src/AdminComponent/CreateRestaurantForm/CreateRestaurantForm.jsx
+++ frontend-food-ordering/src/AdminComponent/CreateRestaurantForm/CreateRestaurantForm.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const CreateRestaurantForm = () => {
   return (
-    <div>CreateRestaurantForm</div>
+    <div className='py-10 lg:flex item-center justify-center min-h-screen'>
+      <h1 className='font-bold text-2xl text-center'>
+        Add New Restaurant
+      </h1>
+    </div>
   )
 }
 
